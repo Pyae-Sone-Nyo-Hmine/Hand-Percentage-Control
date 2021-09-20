fa7fc7731cf492cb9775b0acf3c3a94d96b9c4e7
Pyae-Sone-Nyo-Hmine <83141660+Pyae-Sone-Nyo-Hmine@users.noreply.github.com>
2021-09-19 21:10:44 -0400

    file updated

--- main.py
+++ main.py
@@ -2,6 +2,16 @@ import mediapipe as mp
 import cv2
 import math
 import numpy as np
+import socket
+
+server = False
+
+if server:
+    s = socket.socket()
+    hostname = socket.gethostname()
+    port = 5050
+    print(hostname + " waiting for connection through port " + str(port))
+    s.connect((hostname, port))
 
 # initializing hand detection from library
 mp_drawing = mp.solutions.drawing_utils
@@ -43,23 +53,39 @@ while True:
         distance = int((distance_1 / distance_2) * 60) - 15
         distance = np.clip(distance, 0, 100)
 
+        midpoint = int((thumb[0] + index_finger[0]) / 2), int((thumb[1] + index_finger[1]) / 2)
+
+        midpoint_distance = math.sqrt(((int(width / 2) - midpoint[0]) ** 2) + ((int(height / 2) - midpoint[1]) ** 2))
+
         # draw points on showing opencv gui
         for hand_landmarks in results.multi_hand_landmarks:
             mp_drawing.draw_landmarks(image, hand_landmarks, mp_hands.HAND_CONNECTIONS)
 
-        # draw extra dots and lines for extra clarification
-        cv2.circle(image, thumb, 0, (255, 0, 0), 20)
-        cv2.circle(image, index_finger, 0, (255, 0, 0), 20)
-        cv2.line(image, thumb, index_finger, (255, 0, 0), 3)
+        if midpoint_distance < 90:
+
+            # draw extra dots and lines for extra clarification
+            cv2.circle(image, thumb, 0, (255, 0, 0), 20)
+            cv2.circle(image, index_finger, 0, (255, 0, 0), 20)
+            cv2.line(image, thumb, index_finger, (255, 0, 0), 3)
+
+            # flip the image
+            image = cv2.flip(image, 1)
+            cv2.putText(image, str(distance), (60, 70), cv2.FONT_HERSHEY_SIMPLEX, 2, (0, 165, 255), 3, cv2.LINE_AA)
+
+        else:
+            image = cv2.flip(image, 1)
+            cv2.putText(image, "Move hand to center", (40, 40), cv2.FONT_HERSHEY_SIMPLEX, 1, (255, 165, 0), 2,
+                        cv2.LINE_AA)
+            distance = None
 
     else:
-        distance = 0
+        image = cv2.flip(image, 1)
+        distance = None
 
-    # to flip the image
-    image = cv2.flip(image, 1)
+    if server:
+        s.send(str(distance).encode())
 
-    cv2.putText(image, str(distance), (60, 70), cv2.FONT_HERSHEY_SIMPLEX, 2, (0, 165, 255), 3, cv2.LINE_AA)
-
-    cv2.imshow("Hand", image)
+    cv2.imshow("Hand Detection", image)
 
     cv2.waitKey(1)
+
